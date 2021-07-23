a288c7a6e7735a46edca4e3b21e8ce9082b9a54a
Jhonatan Eduardo <jhonatanepp@gmail.com>
2021-07-22 22:58:29 -0300

    test GET and POST Person

--- base/test/tests_person.py
+++ base/test/tests_person.py
@@ -7,8 +7,8 @@ from rest_framework import status
 
 class PersonTestCase(APITestCase):
 
-    def test_create_person_no_user_no_company_no_photo(self):
-        """ Test POST Person no user, company and photo """
+    def test_POST_person(self):
+        """ Test POST Person """
         url = reverse('list_create_person')
         data = {
             "first_name": "First Name TestCase",
@@ -19,42 +19,7 @@ class PersonTestCase(APITestCase):
             "cpf": "7878",
             "andress": {
                 "type": "residential",
-                "zip_code": "",
                 "street": "Street TestCase",
-                "number": "",
-                "complement": "",
-                "district": "",
-                "city": "",
-                "state": "",
-                "country": "",
-            }
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_create_person_with_user_with_company_with_photo(self):
-        """ Test POST Person with user, company and photo """
-        url = reverse('list_create_person')
-        data = {
-            "first_name": "First Name TestCase",
-            "last_name": "Last Name TestCase",
-            "date_of_birth": "1991-07-13",
-            "gender": "m",
-            "type": "voluntary",
-            "photo": False,
-            "cpf": "7878",
-            "user": False,
-            "company": False,
-            "andress": {
-                "type": "residential",
-                "zip_code": "",
-                "street": "Street TestCase",
-                "number": "",
-                "complement": "",
-                "district": "",
-                "city": "",
-                "state": "",
-                "country": "",
             }
         }
         response = self.client.post(url, data, format='json')
