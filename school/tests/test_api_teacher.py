e567ae185058dbb7a1f1fbe88b9973288bd95ca3
Jhonatan Eduardo <jhonatanepp@gmail.com>
2021-07-22 23:59:37 -0300

    add app school

---
+++ school/tests/test_api_teacher.py
@@ -0,0 +1,20 @@
+from rest_framework.test import APITestCase
+
+
+class TeacherCreateTest:
+    def setUp(self):
+        pass
+
+
+class TeacherTestCase(TeacherCreateTest, APITestCase):
+    def test_teacher_post(self):
+        self.fail("Test Teacher Post Fail")
+
+    def test_teacher_list(self):
+        self.fail("Test Teacher List Fail")
+
+    def test_teacher_get(self):
+        self.fail("Test Teacher Get Fail")
+
+    def test_teacher_update(self):
+        self.fail("Test Teacher Post Fail")
